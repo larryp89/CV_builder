baf8bda13ca686721bfd1a5c2006f06db586c1dd
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-10-08 20:17:30 +0100

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Sidebar from "./sidebar";
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
