f73fba7c4f795fd2be9a97e3475b195037fc67d3
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-10-24 16:53:52 +0100

    Add save as PDF functionality

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -5,20 +5,31 @@ import PrevEducation from "./PrevEducation";
 import PrevWorkExp from "./PrevWorkExp";
 import PrevTechnicalSkills from "./PreviewTechnicalSkills";
 import PrevOtherSkills from "./PrevOtherSkills";
+import html2pdf from "html2pdf.js";
 
 function Preview({ defaultForm, detailsForm }) {
+  const saveAsPDF = () => {
+    const element = document.getElementById("preview-section"); // Target the preview section
+    html2pdf().from(element).save(); // Generate the PDF
+  };
+
   return (
-    <div className="preview">
-      <PrevPersonal detailsForm={detailsForm} defaultForm={defaultForm} />
-      <PrevContact detailsForm={detailsForm} defaultForm={defaultForm} />
-      <PrevEducation detailsForm={detailsForm} defaultForm={defaultForm} />
-      <PrevTechnicalSkills
-        detailsForm={detailsForm}
-        defaultForm={defaultForm}
-      />
-      <PrevWorkExp detailsForm={detailsForm} defaultForm={defaultForm} />
-      <PrevOtherSkills detailsForm={detailsForm} defaultForm={defaultForm} />
-    </div>
+    <>
+      <button className="button save-form" onClick={saveAsPDF}>
+        Save as PDF
+      </button>
+      <div id="preview-section" className="preview">
+        <PrevPersonal detailsForm={detailsForm} defaultForm={defaultForm} />
+        <PrevContact detailsForm={detailsForm} defaultForm={defaultForm} />
+        <PrevEducation detailsForm={detailsForm} defaultForm={defaultForm} />
+        <PrevTechnicalSkills
+          detailsForm={detailsForm}
+          defaultForm={defaultForm}
+        />
+        <PrevWorkExp detailsForm={detailsForm} defaultForm={defaultForm} />
+        <PrevOtherSkills detailsForm={detailsForm} defaultForm={defaultForm} />
+      </div>
+    </>
   );
 }
 
